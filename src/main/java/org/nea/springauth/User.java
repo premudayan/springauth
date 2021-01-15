ba9f308fab5a9cf2111d9729d9a6bc05a2b2038a
UPrem <prem_udayan@yahoo.com>
2021-01-14 21:58:58 -0500

    Created User class and connected to mysql database to create the users table.(Note: create the db in mysql before running the code.)

---
+++ src/main/java/org/nea/springauth/User.java
@@ -0,0 +1,28 @@
+package org.nea.springauth;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(nullable = false, length = 30)
+    private String firstName;
+
+    @Column(nullable = false, length = 30)
+    private String lastName;
+
+
+}
---
+++ src/main/java/org/nea/springauth/UserRepository.java
@@ -0,0 +1,7 @@
+package org.nea.springauth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
